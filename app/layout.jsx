93546128a078f29c8a23d904fab4b3c5d6524a41
Vishal Gori <95775403+Vishal-Gori@users.noreply.github.com>
2024-02-11 16:31:11 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,34 +1,39 @@
 import '@styles/globals.css';
-import Head from 'next/head';
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
-
+import Head from 'next/head'; // Import the Head component
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover and Share AI Prompts"
+  title: "Promptopia",
+  description: "Discover and Share AI Prompts"
 }
 
 const RootLayout = ({ children }) => {
-  return (
-    <html lang='en'>
-      <Head>
-        <script defer data-domain="promptopia-pink-omega.vercel.app" src="https://plausible.io/js/script.js"></script>
-      </Head>
-      <body>
-        <Provider>
-          <div className="main">
-            <div className='gradient'/>
-          </div>
-
-          <main className="app">
-            <Nav/>
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  )
+ return (
+  <>
+    <Head>
+      {/* Plausible Analytics Script */}
+      <script
+        defer
+        data-domain="promptopia-pink-omega.vercel.app"
+        src="https://plausible.io/js/script.js"
+      ></script>
+    </Head>
+    <html lang='en'>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className='gradient'/>
+          </div>
+          <main className="app">
+            <Nav/>
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  </>
+ )
 }
 
-export default RootLayout
+export default RootLayout;
