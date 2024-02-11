1bcb3be2d16dee4758ca23d553d8e4b9e94e8bfc
Vishal Gori <95775403+Vishal-Gori@users.noreply.github.com>
2024-02-11 17:15:33 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import '@styles/globals.css';
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
-import Head from 'next/head'; // Import the Head component
+
 
 export const metadata = {
   title: "Promptopia",
@@ -10,30 +10,21 @@ export const metadata = {
 
 const RootLayout = ({ children }) => {
  return (
-  <>
-    <Head>
-      {/* Plausible Analytics Script */}
-      <script
-        defer
-        data-domain="promptopia-pink-omega.vercel.app"
-        src="https://plausible.io/js/script.js"
-      ></script>
-    </Head>
-    <html lang='en'>
-      <body>
-        <Provider>
-          <div className="main">
-            <div className='gradient'/>
-          </div>
-          <main className="app">
-            <Nav/>
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  </>
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className="main">
+          <div className='gradient'/>
+        </div>
+
+        <main className="app">
+          <Nav/>
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
  )
 }
 
-export default RootLayout;
+export default RootLayout
