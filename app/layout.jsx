7ada241e2b02d24558fd04b781598bcf8a23a631
Vishal Gori <95775403+Vishal-Gori@users.noreply.github.com>
2024-02-11 16:10:07 +0530

    Plausible Analytics added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import '@styles/globals.css';
+import Head from 'next/head';
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
 
@@ -11,18 +12,21 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
-        <body>
-            <Provider>
-                <div className="main">
-                    <div className='gradient'/>
-                </div>
+      <Head>
+        <script defer data-domain="promptopia-pink-omega.vercel.app" src="https://plausible.io/js/script.js"></script>
+      </Head>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className='gradient'/>
+          </div>
 
-                <main className="app">
-                    <Nav/>
-                    {children}
-                </main>
-            </Provider>
-        </body>
+          <main className="app">
+            <Nav/>
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
   )
 }
